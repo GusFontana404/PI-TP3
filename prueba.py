53bb5a556211386b35c01a6cde33d3b91c5de157
Gustavo Fontana <117466366+GusFontana404@users.noreply.github.com>
2023-12-05 20:17:56 -0300

    Agrego el archivo para probar las funciones del ej_1

---
+++ prueba.py
@@ -0,0 +1,12 @@
+# --- PRUEBA -------------------------------------------------------------------------------
+
+#Cargar videos
+video_1,video_2,video_3,video_4 = 'ruta_1.mp4','ruta_2.mp4','ruta_3.mp4','ruta_4.mp4'
+
+#Ejecutar función para cada video y mostrar resultados
+video_segmentado(video_1)
+video_segmentado(video_2)
+video_segmentado(video_3)
+video_segmentado(video_4)
+
+# --- NOTA: Remplazar la ruta 
